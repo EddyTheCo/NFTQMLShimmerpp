6be0f44d906c0aaf8f4a4d5a9eca5d0666a74dd6
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-10-19 11:12:26 +0200

    Fixes (#7)
    * Added the resources in target initialization
    
    * Separate the main application CMake's
    
    * Do not set the package version when finding
    
    * Install the plugin folder also
    
    * Diferentiate between QML and CPP types in the naming
    
    * Install the plugin target for static linking
    
    * Use tag v0.1.1

--- ExampleApp/main.cpp
+++ ExampleApp/main.cpp
@@ -1,14 +1,8 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-
-// One needs to use a symbol from the QML library to force the linker to link the library
-// https://forum.qt.io/post/762513
-#include "nftmonitor.hpp"
-
 int main(int argc, char *argv[])
 {
-	auto foo=foonftMonitor::fooPrint(); //https://forum.qt.io/post/762513
 	//Creates a Gui application https://doc.qt.io/qt-6/qguiapplication.html#details
 	QGuiApplication app(argc, argv);
 
@@ -18,6 +12,7 @@ int main(int argc, char *argv[])
 	// Add `/esterVtech.com/imports` to the QML Import Path https://doc.qt.io/qt-6/qtqml-syntax-imports.html
 	// So our custom QML module(and the app window.qml) is available to the engine
 	engine.addImportPath("qrc:/esterVtech.com/imports");
+	qDebug()<<engine.importPathList();
 
 	const QUrl url(u"qrc:/esterVtech.com/imports/App/qml/window.qml"_qs);
 
